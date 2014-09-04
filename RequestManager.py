437dc3b2427af4ad8f724a2f28039e49f523b2eb
Viktor <yazexel@gmail.com>
2014-09-03 23:08:25 -0700

    Added first version of RequestManager

---
+++ RequestManager.py
@@ -0,0 +1,47 @@
+import json
+import urllib
+import urllib2
+
+class RequestManager:
+
+	def __init__(self):
+		pass
+
+	def login(self, username, password):
+		
+		user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
+
+		url = 'https://www.okcupid.com/login'
+
+		values = {'username' : username,
+		'password' : password,
+		'okc_api' : '1' }
+		headers = { 'User-Agent' : user_agent }
+
+		data = urllib.urlencode(values)
+		req = urllib2.Request(url, data, headers)
+		response = urllib2.urlopen(req)
+		the_page = response.read()
+		print the_page
+
+		jsonValue = json.loads(the_page)
+
+		userIdValue = jsonValue['userid']
+
+		if userIdValue == 10:
+			print 'userid is 10'
+		else:
+			print 'userid is ' + str(userIdValue)
+
+			if userIdValue:
+				print 'success'
+			else:
+				print 'error'
+
+
+requestAPI = RequestManager()
+requestAPI.login('yazexel@gmail.com', 'testpasswo')
+
+
+
+
