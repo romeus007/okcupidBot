c5967f6e51fb9ed24d139d74a9588d813cdd77e0
romeus99 <romeus007@mail.ru>
2014-09-07 18:17:10 -0700

    Understanding current code + login cred-s

--- RequestManager.py
+++ RequestManager.py
@@ -1,15 +1,15 @@
-import json
-import urllib
+import json   #exposes an API
+import urllib   
 import urllib2
 
 class RequestManager:
 
 	def __init__(self):
-		pass
+		pass                      #just a placeholder, why?
 
 	def login(self, username, password):
 		
-		user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
+		user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'  #so that browser identifies itself as a version of Internet Explorer
 
 		url = 'https://www.okcupid.com/login'
 
@@ -18,25 +18,18 @@ class RequestManager:
 		'okc_api' : '1' }
 		headers = { 'User-Agent' : user_agent }
 
-		data = urllib.urlencode(values)
-		req = urllib2.Request(url, data, headers)
-		response = urllib2.urlopen(req)
-		the_page = response.read()
+		data = urllib.urlencode(values)            # Data encoding, why did we encode with "urllib" and not "urllib2"
+		req = urllib2.Request(url, data, headers)  # Python module for fetching URLs(represents HTTP request you're making)
+		response = urllib2.urlopen(req)            # returns a response object for the URL requested
+		the_page = response.read()                 # reads response object
 		print the_page
 
-		jsonValue = json.loads(the_page)
+		jsonValue = json.loads(the_page) #takes a JSON string and returns it as a Python data structure
 
 		userIdValue = jsonValue['userid']
 
-		if userIdValue == 10:
-			print 'userid is 10'
-		else:
-			print 'userid is ' + str(userIdValue)
-
-			if userIdValue:
-				print 'success'
-			else:
-				print 'error'
+requestAPI = RequestManager()
+requestAPI.login("romeus99","password348127")
 
 
 
